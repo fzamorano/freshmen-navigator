fc53f4d5c64bfcaed4c48cd56b43c4b6536689e1
Dave14586 <Dave14586@7292dc77-05f0-d909-0dcd-b85113708c7a>
2012-12-17 17:21:45 +0000

    naviPoints

--- trunk/ADMUC-ErstieNavigator/src/com/piq/erstieNavi/NaviPointsActivity.java
+++ trunk/ADMUC-ErstieNavigator/src/com/piq/erstieNavi/NaviPointsActivity.java
@@ -8,7 +8,9 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.piq.erstieNavi.model.Building;
@@ -16,76 +18,117 @@ import com.piq.erstieNavi.services.BuildingsManager;
 
 public class NaviPointsActivity extends Activity {
 	
+private Location recLoc = null;
+private CheckBox locCB;
+	
+	
 	public void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.navi_points);
 		
+		
+		locCB = (CheckBox) this.findViewById(R.id.locCheckBox);
+		
+		locCB.setOnClickListener(new View.OnClickListener()
+		{
+			TextView t1 = (TextView) findViewById(R.id.longi);
+			EditText e1 = (EditText) findViewById(R.id.longiE);
+			TextView t2 = (TextView) findViewById(R.id.lat);
+			EditText e2 = (EditText) findViewById(R.id.latE);
+			
+			public void onClick(View v) {
+				
+				if (locCB.isChecked())
+				{
+					t1.setEnabled(false);
+					e1.setEnabled(false);
+					t2.setEnabled(false);
+					e2.setEnabled(false);
+					
+					receiveLocation();
+					
+					Double lat = recLoc.getLatitude();
+					Double longi = recLoc.getLongitude();
+					
+					toastL("Long.:" + longi.toString() + "/" + "Lat.:" + lat.toString());
+				}
+				else
+				{
+					t1.setEnabled(true);
+					e1.setEnabled(true);
+					t2.setEnabled(true);
+					e2.setEnabled(true);
+				}
+				
+			}
+		});
+		
 	}
 	
-	public void save(View v) {
+	public void save(View v) 
+	{
 		EditText abbrevE = (EditText) findViewById(R.id.abbrevE);
-		EditText longiE = (EditText) findViewById(R.id.longiE);
-		EditText latE = (EditText) findViewById(R.id.latE);
 		EditText nameE = (EditText) findViewById(R.id.nameE);
-		if (!nameE.getText().toString().equals("") && !abbrevE.getText().toString().equals("") && !latE.getText().toString().equals("") && !longiE.getText().toString().equals("")) {
-			Building b = new Building(nameE.getText().toString(), abbrevE.getText().toString(), Double.parseDouble(latE.getText().toString()), Double.parseDouble(longiE.getText().toString()));
-			BuildingsManager.getInstance().addBuilding(b);
-			toastL("New Point added: " + b.toString());
-			Intent i = new Intent(v.getContext(), Main.class);
-			startActivity(i);
-		} else {
+		
+		if (!nameE.getText().toString().equals("") && !abbrevE.getText().toString().equals(""))
+		{
+			if(!locCB.isChecked())
+			{
+				EditText longiE = (EditText) findViewById(R.id.longiE);
+				EditText latE = (EditText) findViewById(R.id.latE);
+				
+				if(!latE.getText().toString().equals("") && !longiE.getText().toString().equals(""))
+				{
+					Building b = new Building(nameE.getText().toString(), 
+											  abbrevE.getText().toString(), 
+											  Double.parseDouble(latE.getText().toString()), 
+											  Double.parseDouble(longiE.getText().toString()));
+					BuildingsManager.getInstance().addBuilding(b);
+					toastL("New Point added: " + b.toString());
+					Intent i = new Intent(v.getContext(), Main.class);
+					startActivity(i);
+				}
+				else
+				{
+					toastL("Not every field was filled. Please fill every field and try again! Thanks.");
+				}
+			}
+			else
+			{
+				Building b = new Building(nameE.getText().toString(), 
+										  abbrevE.getText().toString(), recLoc.getLatitude(),
+										  recLoc.getLongitude());
+				BuildingsManager.getInstance().addBuilding(b);
+				toastL("New Point added: " + b.toString());
+				Intent i = new Intent(v.getContext(), Main.class);
+				startActivity(i);
+			}
+		}
+		else
+		{
 			toastL("Not every field was filled. Please fill every field and try again! Thanks.");
 		}
+				
+					
 	}
 	
 	public void toastL(String msg) {
 		Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
-	} // end toast
+	}
 	
-	public void receiveLocation(View v) {
-		// getCurrentLocation();
-		// performReverseGeocodingInBackground();
-		// while (geoCodeResult == null) {
-		// }
-		// Geocoder geocoder;
-		// String bestProvider;
-		// List<Address> user = null;
-		double lat = 0;
-		double lng = 0;
-		
-		// LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-		//
-		// Criteria criteria = new Criteria();
-		// bestProvider = lm.getBestProvider(criteria, false);
-		// Location location = lm.getLastKnownLocation(bestProvider);
-		//
-		// if (location == null) {
-		// toastL("Location Not found");
-		// } else {
-		// geocoder = new Geocoder(this);
-		// try {
-		// user = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-		// lat = (double) user.get(0).getLatitude();
-		// lng = (double) user.get(0).getLongitude();
-		// System.out.println(" DDD lat: " + lat + ",  longitude: " + lng);
-		//
-		// } catch (Exception e) {
-		// e.printStackTrace();
-		// }
-		// }
+	public void receiveLocation() 
+	{
 		LocationManager locManager;
 		locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 500.0f, locationListener);
-		Location location = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		if (location != null) {
-			lat = location.getLatitude();
-			lng = location.getLongitude();
-		}
-		EditText longiE = (EditText) findViewById(R.id.longiE);
-		EditText latE = (EditText) findViewById(R.id.latE);
-		longiE.setText("" + lat);
-		latE.setText("" + lng);
+		
+		recLoc = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		
+		int lat = (int) (recLoc.getLatitude() *1E6);
+		int longi = (int) (recLoc.getLongitude() *1E6);
+		recLoc.setLatitude((double)lat /1E6);
+		recLoc.setLongitude((double)longi/1E6);
 	}
 	
 	private final LocationListener locationListener = new LocationListener() {
@@ -114,4 +157,5 @@ public class NaviPointsActivity extends Activity {
 			latLongString = "No location found";
 		}
 	}
+	
 }
